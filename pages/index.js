8239f967e57309ffb685c4ef2cd39cf5a3ad2e2f
LORRAN HENRIQUE <101062573+Lorranlhss@users.noreply.github.com>
2024-12-26 00:14:07 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1>Jaqueline, eu amo você. Se você me ama, dá uma risadinha :D !</h1>
+function Home() {
+  return <h1>Jaqueline, eu amo você. Se você me ama, dá uma risadinha :D !</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indendatação errada !");
 }
 
 export default Home;
